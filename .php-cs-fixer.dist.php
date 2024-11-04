53511fa7c55163e4a14dfb59b54d709c375341ed
Toshiyuki FUJITA <kalibora@gmail.com>
2024-11-04 17:08:50 +0900

    Add QA tools

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,16 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('var')
+;
+
+return (new PhpCsFixer\Config())
+    ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
+    ->setRules([
+        '@Symfony' => true,
+        'concat_space' => false,
+        'global_namespace_import' => true,
+    ])
+    ->setFinder($finder)
+;
